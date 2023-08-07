bcbeea4c9e68336e36dea0a30a8e785546eb26b8
tangpan <xidear@126.com>
2023-08-07 08:35:34 +0800

    增加 Text2Gateway.php ,对应Poi2.0

---
+++ src/Gateways/Search/Text2Gateway.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: li
+ * Date: 2017/12/8
+ * Time: 17:46
+ */
+
+namespace Reprover\Amap\Gateways\Search;
+
+
+use Reprover\Amap\Gateways\Gateway;
+use Reprover\Amap\Results\SearchResult;
+
+class Text2Gateway extends Gateway
+{
+
+    protected $uri="https://restapi.amap.com/v5/place/text";
+    protected $rules=[
+        "key"=>"required",
+        "keywords"=>"switch",
+        "types"=>"switch",
+        "region"=>"nullable",
+        "city_limit"=>"nullable",
+        "show_fields"=>"nullable",
+        "page_size"=>"nullable",
+        "page_num"=>"nullable",
+        "sig"=>"nullable",
+        "output"=>"nullable",
+        "callback"=>"nullable"
+    ];
+
+    /**
+     * @return SearchResult
+     * @throws \Reprover\Amap\Exceptions\CannotParseResponseException
+     * @throws \Reprover\Amap\Exceptions\HttpException
+     */
+    public function ask()
+    {
+//        $this->base_config->set("https",true);
+        return new SearchResult($this->sendRequest());
+    }
+}
